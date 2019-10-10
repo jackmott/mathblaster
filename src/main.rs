61ffba67e2cd2588ebd66ebe90830e1716a31bee
Jack Mott <jack.mott@gmail.com>
2019-10-10 17:14:05 -0500

    draw a spaceship

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use ggez;
 use ggez::event;
-use ggez::graphics;
+use ggez::graphics::{self,Color};
 use ggez::nalgebra as na;
 use ggez::{Context,GameResult};
+use std::path;
+use std::env;
 
-
-enum AlienType {
+enum AlienKind {
     Add,
     Subtract,
     Multiply,
@@ -14,11 +15,32 @@ enum AlienType {
 
 
 struct Alien {
-    type: AlienType,
-    pos: na::Point2,
+    kind: AlienKind,
+    pos: na::Point2<f32>,    
     img: graphics::Image,
     num1: i32,
     num2: i32
+}
+
+impl Alien {
+
+    fn new(ctx : &mut Context, kind: AlienKind) -> Alien {       
+        Alien {
+            kind: kind,
+            pos: na::Point2::new(0.0f32,0.0f32),
+            img: graphics::Image::new(ctx, "/blueships1.png").unwrap(),
+            num1: 2,
+            num2: 2,
+        }        
+    }
+
+    fn update(&mut self, ctx:&mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx:&mut Context) -> GameResult {
+        graphics::draw(ctx,&self.img,(self.pos,0.0,Color::from((255,255,255,255))))
+    }
 }
 
 
@@ -27,38 +49,36 @@ struct MainState {
 }
 
 impl MainState {
-    fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x:0.0};
-        Ok(s)
+    fn new(ctx: &mut Context) -> GameResult<MainState> {        
+        Ok( MainState { alien: Alien::new(ctx,AlienKind::Add) } )
     }
 }
 
 impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx:&mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+    fn update(&mut self, _ctx:&mut Context) -> GameResult {      
         Ok(())
     }
 
     fn draw(&mut self, ctx:&mut Context) -> GameResult {
         graphics::clear(ctx, [0.1,0.2,0.3,1.0].into());
-
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            na::Point2::new(0.0,0.0),
-            100.0,
-            2.0,
-            graphics::WHITE,
-            )?;
-        graphics::draw(ctx,&circle,(na::Point2::new(self.pos_x,380.0),))?;
+        self.alien.draw(ctx);
         graphics::present(ctx)?;
         Ok(())
     }
 }
 
 pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super simple","ggez");
+    let resource_dir = 
+        if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+            let mut path = path::PathBuf::from(manifest_dir);
+            path.push("resources");
+            path
+        } else {
+            path::PathBuf::from("./resources")
+        };
+
+    let cb = ggez::ContextBuilder::new("super simple","ggez").add_resource_path(resource_dir);
     let (ctx,event_loop) = &mut cb.build()?;
-    let state = &mut MainState::new()?;
+    let state = &mut MainState::new(ctx)?;
     event::run(ctx,event_loop,state)
 }
