192e3c652f34d4daf695320cd197afc0318ad0cc
Jack Mott <jack.mott@gmail.com>
2019-10-10 22:24:19 -0500

    resizeable

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use ggez;
+use ggez::conf;
 use ggez::event;
 use ggez::graphics::{self, Color, DrawParam};
 use ggez::nalgebra as na;
 use ggez::timer;
 use ggez::{Context, GameResult};
+use rand::*;
 use std::env;
 use std::path;
-use rand::*;
+
+fn worldToScreen(p: na::Point2<f32>, width: f32, height: f32) -> na::Point2<f32> {
+    na::Point2::new(p[0] * width, p[1] * height)
+}
 
 struct Assets {
     blue_ship_img: graphics::Image,
+    num_font: graphics::Font,
+    turret: graphics::Image,
 }
 
 impl Assets {
     fn new(ctx: &mut Context) -> Assets {
         Assets {
             blue_ship_img: graphics::Image::new(ctx, "/blueships1.png").unwrap(),
+            num_font: graphics::Font::new(ctx, "/dejamono.ttf").unwrap(),
+            turret: graphics::Image::new(ctx, "/turret.png").unwrap(),
         }
     }
 }
@@ -34,27 +43,32 @@ struct LevelSpec {
     operations: Vec<Operation>,
 }
 
+struct Turret {
+    rotation: f32,
+}
+
+impl Turret {
+    fn update(&mut self, ctx: &mut Context, dt: std::time::Duration) {}
+
+    fn draw(&mut self, ctx: &mut Context, assets: &mut Assets) {
+        let param = DrawParam::new()
+            .color(Color::from((255, 255, 255, 255)))
+            .dest(na::Point2::new(0.0, 100.0));
+        let _ = graphics::draw(ctx, &assets.turret, param);
+    }
+}
+
 struct Alien {
     operation: Operation,
     speed: f32,
     pos: na::Point2<f32>,
-    num1: i32,
-    num2: i32,
+    text: graphics::Text,
+    answer: i32,
 }
 
 impl Alien {
-    fn new(operation: Operation, speed: f32) -> Alien {
-        Alien {
-            operation: operation,
-            speed: speed,
-            pos: na::Point2::new(100.0f32, 100.0f32),
-            num1: 2,
-            num2: 2,
-        }
-    }
-
     fn update(&mut self, ctx: &mut Context, dt: std::time::Duration) {
-        let sec = dt.as_millis() as f32 / 1000.0;
+        let sec = dt.as_millis() as f32 / 100000.0;
         self.pos = self.pos + na::Vector2::new(0.0, self.speed * sec);
     }
 
@@ -63,11 +77,23 @@ impl Alien {
         let scale = desired_size / (assets.blue_ship_img.width() as f32);
         let params = DrawParam::new()
             .color(Color::from((255, 255, 255, 255)))
-            .dest(self.pos)
+            .dest(worldToScreen(
+                self.pos,
+                graphics::size(ctx).0,
+                graphics::size(ctx).1,
+            ))
             .scale(na::Vector2::new(scale, scale))
             .offset(na::Point2::new(0.5, 0.5))
             .rotation(3.14159 / 2.0);
         let _ = graphics::draw(ctx, &assets.blue_ship_img, params);
+        let text_param = DrawParam::new()
+            .color(Color::from((255, 255, 255, 255)))
+            .dest(worldToScreen(
+                self.pos,
+                graphics::size(ctx).0,
+                graphics::size(ctx).1,
+            ));
+        let _ = graphics::draw(ctx, &self.text, text_param);
     }
 }
 
@@ -76,7 +102,8 @@ struct MainState {
     aliens: Vec<Alien>,
     assets: Assets,
     levels: Vec<LevelSpec>,
-    current_level:usize
+    current_level: usize,
+    turret: Turret,
 }
 
 impl MainState {
@@ -324,30 +351,40 @@ impl MainState {
             },
         ];
 
-        let assets = Assets::new(ctx);        
+        let assets = Assets::new(ctx);
         Ok(MainState {
             dt: std::time::Duration::new(0, 0),
-            aliens: gen_aliens(&levels[0]),
+            aliens: gen_aliens(&levels[0], &assets.num_font),
             assets: assets,
             levels: levels,
-            current_level: 0
+            current_level: 0,
+            turret: Turret { rotation: 0.0 },
         })
     }
 }
 
-fn gen_aliens(level_spec:&LevelSpec) -> Vec<Alien> {    
+fn gen_aliens(level_spec: &LevelSpec, font: &graphics::Font) -> Vec<Alien> {
     let mut aliens = Vec::new();
     let mut rng = rand::thread_rng();
     let op_count = level_spec.operations.len();
-    for i in 0 .. 20 {
-        let alien = 
-            Alien {
-                    operation: level_spec.operations[rng.gen_range(0,op_count)],
-                    speed: level_spec.speed,
-                    pos: na::Point2::new(rng.gen_range(0.0,800.0),rng.gen_range(-(i+1) as f32 *100.0,0.0)),
-                    num1: rng.gen_range(0,level_spec.max_number),
-                    num2: rng.gen_range(0,level_spec.max_number) //todo with division add some logic
-            };
+    for i in 0..20 {
+        let operation = level_spec.operations[rng.gen_range(0, op_count)];
+        let num1 = rng.gen_range(0, level_spec.max_number);
+        let num2 = rng.gen_range(0, level_spec.max_number); //todo with division add some logic
+        let (answer, op) = match operation {
+            Operation::Add => (num1 + num2, "+"),
+            Operation::Subtract => (num1 - num2, "-"),
+            Operation::Multiply => (num1 * num2, "x"),
+            Operation::Divide => (num1 / num2, "/"),
+        };
+        let text = num1.to_string() + op + &num2.to_string();
+        let alien = Alien {
+            operation: operation,
+            speed: level_spec.speed,
+            pos: na::Point2::new(rng.gen_range(0.0, 1.0), -i as f32 * 0.1),
+            text: graphics::Text::new((text, *font, 24.0)),
+            answer: answer,
+        };
         aliens.push(alien);
     }
     aliens
@@ -357,18 +394,27 @@ impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         self.dt = timer::delta(ctx);
         for alien in &mut self.aliens {
-            alien.update(ctx, self.dt);            
+            alien.update(ctx, self.dt);
         }
+        self.turret.update(ctx, self.dt);
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
         for alien in &mut self.aliens {
-            alien.draw(ctx, &mut self.assets);         
-        }        
+            alien.draw(ctx, &mut self.assets);
+        }
+        self.turret.draw(ctx, &mut self.assets);
         graphics::present(ctx)?;
         Ok(())
+    }
+
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        println!("Resized screen to {}, {}", width, height);
+
+        let new_rect = graphics::Rect::new(0.0, 0.0, width as f32, height as f32);
+        graphics::set_screen_coordinates(ctx, new_rect).unwrap();
     }
 }
 
@@ -381,7 +427,14 @@ pub fn main() -> GameResult {
         path::PathBuf::from("./resources")
     };
 
-    let cb = ggez::ContextBuilder::new("super simple", "ggez").add_resource_path(resource_dir);
+    let cb = ggez::ContextBuilder::new("super simple", "ggez")
+        .add_resource_path(resource_dir)
+        .window_mode(
+            conf::WindowMode::default()
+                .fullscreen_type(conf::FullscreenType::Windowed)
+                .resizable(true),
+        );
+
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut MainState::new(ctx)?;
     event::run(ctx, event_loop, state)
